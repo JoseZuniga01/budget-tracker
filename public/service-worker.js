27794c4308a837cd41329a470a95564b4771e0f5
Jose Zuniga <josezuniga@joses-macbook.myfiosgateway.com>
2022-08-07 14:53:16 -0400

    finsiehd SW for offline use

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,4 @@
-// TODO: implement service worker so that users can use the app offline. The SW
-// will need to cache static assets to display the app offline. Additionally,
-// the SW should cache transaction data, using the cached data as a fallback
-// when the app is used offline. HINT: You should use two caches. One for the
-// static assets such ass html, css, js, images, etc; and another cache for
-// the dynamic data from requests to routes beginning with "/api".
+
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -30,7 +25,6 @@ const FILES_TO_CACHE = [
   });
   
   self.addEventListener("activate", (evt) => {
-    // remove old caches
     evt.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(
